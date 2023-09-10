f35eb7b8d7ebcd054cf4a349ef619a0da5a9b46b
ConnorBieszk <84150127+pyluaVR@users.noreply.github.com>
2023-09-09 22:45:03 -0500

    created batch file

---
+++ main.bat
@@ -0,0 +1,7 @@
+@echo off
+title Gorrila Tag Mod Setup
+Color 27
+echo |--------------------------------|
+echo |    Gorrila Tag Mod Installer   |
+echo |--------------------------------|
+pause <nul
